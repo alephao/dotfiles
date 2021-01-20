bee709f067876924285e5e164ef2222b5da3299f
Aleph Retamal <7674479+alephao@users.noreply.github.com>
2021-01-20 16:24:22 -0300

    add my git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,3 +5,20 @@ if (( $+commands[hub] ))
 then
   alias git=$hub_path
 fi
+
+# My aliases
+alias gb='git branch'
+
+alias gco='git checkout'
+
+alias gclean='git clean -id'
+
+alias gf='git fetch'
+alias gfp='git fetch --prune'
+
+alias glog='git log --oneline'
+alias glol='git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"'
+
+alias gm='git merge'
+
+alias gss='git status -s'
