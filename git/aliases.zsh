1fb98c4b511f6e12c6faa83878f320bca7412ee4
Aleph Retamal <7674479+alephao@users.noreply.github.com>
2021-05-06 12:32:34 -0300

    add more git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,23 +7,50 @@ then
 fi
 
 # My aliases
-alias gb='git branch'
+alias g='git'
 
+# git add
+alias ga='git add'
+alias gaa='git add --all'
+
+# git branch
+alias gb='git branch'
+alias gbd='git branch -D'
+
+# git checkout
 alias gco='git checkout'
 
+# git clean
 alias gclean='git clean -id'
 
+# git diff
 alias gd='git diff'
 
+# git push
 alias gp='git push'
 alias gpu='git push -u origin HEAD'
+alias gpf='git push -f'
 
+# git fetch
 alias gf='git fetch'
 alias gfp='git fetch --prune'
+alias gfpr='gfp && git rebase'
 
+# git log
 alias glog='git log --oneline'
 alias glol='git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"'
+alias gaxe='git log -S'
 
+# git merge
 alias gm='git merge'
+alias gms='git merge --squash'
 
+# git status
 alias gss='git status -s'
+
+# git rebase
+alias gri='git ri'
+
+# git reset
+alias grs='git reset'
+alias grsh='git reset --hard'
